eb8fb34d8eb23a64ec8a94de9948e2c94ed4696d
Ефимов Максим <efimov_m@itstep.academy>
2023-04-30 16:37:07 +0300

    Добавьте файлы проекта.

---
+++ NetParallelApp/Program.cs
@@ -0,0 +1,37 @@
+﻿//Action[] actions = new Action[3] { Counter, Counter, Counter };
+
+//Parallel.Invoke(actions);
+
+//var result = Parallel.For(1, 10, Square);
+//if(!result.IsCompleted)
+//    Console.WriteLine("Loop is break");
+
+List<int> nums = new List<int>() { 5, 4, 6, 8, 9 };
+var result = Parallel.ForEach<int>(nums, Square);
+if (!result.IsCompleted)
+    Console.WriteLine("Loop is break");
+
+void Counter()
+{
+    for(int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} {i}");
+        Thread.Sleep(500);
+    }
+}
+
+void CounterNum(int num)
+{
+    for (int i = 0; i < num; i++)
+    {
+        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId} {i}");
+        Thread.Sleep(500);
+    }
+}
+
+void Square(int num, ParallelLoopState state)
+{
+    if(num == 5)
+        state.Break();
+    Console.WriteLine($"{num} - {num * num}");
+}
